415877ed535f4c0a7502e239d6e38327f9710dcb
Mario Gender <Gender.Mario@outlook.com>
2021-06-25 10:47:54 +0200

    #74 Initial setup for EF and DB

--- EquitesSolution/Startup.cs
+++ EquitesSolution/Startup.cs
@@ -1,17 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Application.Managers;
+using Application.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Persistence;
 
-namespace EquitesSolution
+namespace API
 {
     public class Startup
     {
@@ -25,6 +22,18 @@ namespace EquitesSolution
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DataContext>(opt =>
+            {
+                opt.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
+
+
+            //Add Transient Repositories
+            services.AddTransient<IValueRepository, ValueRepository>();
+
+            //Add Scoped Managers
+            services.AddScoped<IValueManager, ValueManager>();
+
             services.AddControllers();
         }
 
