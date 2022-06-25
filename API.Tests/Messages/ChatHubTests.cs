2fe971650ca5da72014456eec83622773fd5bfb0
Marko Todorovic <todorovic.m@outlook.com>
2022-06-25 15:05:44 +0200

    #309 added tests

---
+++ API.Tests/Messages/ChatHubTests.cs
@@ -0,0 +1,104 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using API.Messages;
+using Application.InfrastructureInterfaces.Security;
+using Application.Models.Comment;
+using Application.ServiceInterfaces;
+using FixtureShared;
+using Microsoft.AspNetCore.SignalR;
+using Moq;
+using NUnit.Framework;
+
+namespace API.Tests.Messages
+{
+    public class ChatHubTests
+    {
+        private Mock<IChatService> _chatServiceMock;
+        private Mock<IUserAccessor> _userAccessorMock;
+        private ChatHub _sut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _chatServiceMock = new Mock<IChatService>();
+            _userAccessorMock = new Mock<IUserAccessor>();
+            _sut = new ChatHub(_chatServiceMock.Object, _userAccessorMock.Object);
+        }
+
+        [Test]
+        [Fixture(FixtureType.WithAutoMoqAndOmitRecursion)]
+        public async Task SendComment_SuccessfullAsync(CommentCreate commentCreate, CommentReturn comment)
+        {
+            _chatServiceMock.Setup(x => x.ApplyComment(commentCreate))
+                .ReturnsAsync(comment);
+
+            var mockClients = new Mock<IHubCallerClients>();
+            _sut.Clients = mockClients.Object;
+
+            mockClients.Setup(m => m.Group(commentCreate.ActivityId.ToString()).SendCoreAsync("ReceiveComment", new[] { comment }, new CancellationToken())).Verifiable();
+
+            // Act
+            await _sut.SendComment(commentCreate);
+
+            // Assert
+            _chatServiceMock.Verify(x => x.ApplyComment(commentCreate), Times.Once);
+            mockClients.Verify(m => m.Group(commentCreate.ActivityId.ToString()).SendCoreAsync("ReceiveComment", new[] { comment }, new CancellationToken()), Times.Once);
+        }
+
+        [Test]
+        [Fixture(FixtureType.WithAutoMoq)]
+        public async Task AddToGroup_SuccessfullAsync(string userName, string groupName, HubCallerContext hubCallerContext)
+        {
+            _userAccessorMock.Setup(x => x.GetUsernameFromAccesssToken())
+                .Returns(userName);
+
+            var mockClients = new Mock<IHubCallerClients>();
+            _sut.Clients = mockClients.Object;
+
+            var mockGroup = new Mock<IGroupManager>();
+            _sut.Groups = mockGroup.Object;
+
+            _sut.Context = hubCallerContext;
+
+            mockGroup.Setup(m => m.AddToGroupAsync(hubCallerContext.ConnectionId, groupName, new CancellationToken())).Verifiable();
+            mockClients.Setup(m => m.Group(groupName).SendCoreAsync("Send", new[] { $"{userName} ćaska" }, new CancellationToken())).Verifiable();
+
+            // Act
+            await _sut.AddToGroup(groupName);
+
+            // Assert
+            _userAccessorMock.Verify(x => x.GetUsernameFromAccesssToken(), Times.Once);
+            mockGroup.Verify(m => m.AddToGroupAsync(hubCallerContext.ConnectionId, groupName, new CancellationToken()), Times.Once);
+            mockClients.Verify(m => m.Group(groupName).SendCoreAsync("Send", new[] { $"{userName} ćaska" }, new CancellationToken()), Times.Once);
+        }
+
+        [Test]
+        [Fixture(FixtureType.WithAutoMoq)]
+        public async Task RemoveFromGroup_SuccessfullAsync(string userName, string groupName, HubCallerContext hubCallerContext)
+        {
+            _userAccessorMock.Setup(x => x.GetUsernameFromAccesssToken())
+                .Returns(userName);
+
+            var mockClients = new Mock<IHubCallerClients>();
+            _sut.Clients = mockClients.Object;
+
+            var mockGroup = new Mock<IGroupManager>();
+            _sut.Groups = mockGroup.Object;
+
+            _sut.Context = hubCallerContext;
+
+            mockGroup.Setup(m => m.RemoveFromGroupAsync(hubCallerContext.ConnectionId, groupName, new CancellationToken())).Verifiable();
+            mockClients.Setup(m => m.Group(groupName).SendCoreAsync("Send", new[] { $"{userName} ode" }, new CancellationToken())).Verifiable();
+
+            // Act
+            await _sut.RemoveFromGroup(groupName);
+
+            // Assert
+            _userAccessorMock.Verify(x => x.GetUsernameFromAccesssToken(), Times.Once);
+            mockGroup.Verify(m => m.RemoveFromGroupAsync(hubCallerContext.ConnectionId, groupName, new CancellationToken()), Times.Once);
+
+            mockClients.Verify(m => m.Group(groupName).SendCoreAsync("Send", new[] { $"{userName} ode" }, new CancellationToken()), Times.Once);
+        }
+    }
+}
+
---
+++ Application.Test/Services/ChatServiceTests.cs
@@ -0,0 +1,108 @@
+﻿using System.Threading.Tasks;
+using Application.Errors;
+using Application.InfrastructureInterfaces.Security;
+using Application.Models.Comment;
+using Application.Services;
+using AutoMapper;
+using DAL;
+using Domain;
+using FixtureShared;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+
+namespace Application.Tests.Services
+{
+    public class ChatServiceTests
+    {
+        private Mock<IUserAccessor> _userAccessorMock;
+        private Mock<IUnitOfWork> _uowMock;
+        private Mock<IMapper> _mapperMock;
+
+        private ChatService _sut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _userAccessorMock = new Mock<IUserAccessor>();
+            _uowMock = new Mock<IUnitOfWork>();
+            _mapperMock = new Mock<IMapper>();
+
+            _sut = new ChatService(_userAccessorMock.Object, _uowMock.Object, _mapperMock.Object);
+        }
+
+        [Test]
+        [Fixture(FixtureType.WithAutoMoqAndOmitRecursion)]
+        public async Task ApplyComment_SuccessfulAsync(CommentCreate commentCreate, Activity activity, User user, CommentReturn commentReturn)
+        {
+
+            // Arrange
+            _uowMock.Setup(x => x.Activities.GetAsync(commentCreate.ActivityId))
+                .ReturnsAsync(activity);
+
+            _userAccessorMock.Setup(x => x.GetUserIdFromAccessToken())
+                .Returns(It.IsAny<int>());
+
+            _uowMock.Setup(x => x.Users.GetAsync(It.IsAny<int>()))
+                .ReturnsAsync(user);
+
+            _uowMock.Setup(x => x.CompleteAsync())
+                .ReturnsAsync(true);
+
+            _mapperMock
+                .Setup(x => x.Map<CommentReturn>(It.IsAny<Comment>()))
+                .Returns(commentReturn);
+
+            // Act
+            var res = await _sut.ApplyComment(commentCreate);
+
+            // Assert
+            res.Match(
+                comment => comment.Should().BeEquivalentTo(commentReturn),
+                err => err.Should().BeNull()
+                );
+
+            _uowMock.Verify(x => x.Activities.GetAsync(commentCreate.ActivityId), Times.Once);
+            _userAccessorMock.Verify(x => x.GetUserIdFromAccessToken(), Times.Once);
+            _uowMock.Verify(x => x.Users.GetAsync(It.IsAny<int>()), Times.Once);
+            _uowMock.Verify(x => x.CompleteAsync(), Times.Once);
+        }
+
+        [Test]
+        [Fixture(FixtureType.WithAutoMoqAndOmitRecursion)]
+        public async Task ApplyComment_ActivityNotFoundAsync(CommentCreate commentCreate, Activity activity, User user, CommentReturn commentReturn)
+        {
+
+            // Arrange
+            _uowMock.Setup(x => x.Activities.GetAsync(commentCreate.ActivityId))
+                .ReturnsAsync((Activity)null);
+
+            _userAccessorMock.Setup(x => x.GetUserIdFromAccessToken())
+                .Returns(It.IsAny<int>());
+
+            _uowMock.Setup(x => x.Users.GetAsync(It.IsAny<int>()))
+                .ReturnsAsync(user);
+
+            _uowMock.Setup(x => x.CompleteAsync())
+                .ReturnsAsync(true);
+
+            _mapperMock
+                .Setup(x => x.Map<CommentReturn>(It.IsAny<Comment>()))
+                .Returns(commentReturn);
+
+            // Act
+            var res = await _sut.ApplyComment(commentCreate);
+
+            // Assert
+            res.Match(
+                comment => comment.Should().BeNull(),
+                err => err.Should().BeOfType<NotFound>()
+                );
+
+            _uowMock.Verify(x => x.Activities.GetAsync(commentCreate.ActivityId), Times.Once);
+            _userAccessorMock.Verify(x => x.GetUserIdFromAccessToken(), Times.Never);
+            _uowMock.Verify(x => x.Users.GetAsync(It.IsAny<int>()), Times.Never);
+            _uowMock.Verify(x => x.CompleteAsync(), Times.Never);
+        }
+    }
+}
